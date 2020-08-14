2e5e3ff85af5eb95cc3dc8fa02530e230985cd86
l0oktaan <looktaan.tc3@gmail.com>
2020-08-14 15:09:59 +0700

    140863

--- app/Http/Middleware/FrameHeadersMiddleware.php
+++ app/Http/Middleware/FrameHeadersMiddleware.php
@@ -13,12 +13,33 @@ class FrameHeadersMiddleware
      * @param  \Closure  $next
      * @return mixed
      */
+    // public function handle($request, Closure $next)
+    // {
+    //     //return $next($request);
+    //     $response = $next($request);
+    //     // $response->header('X-Frame-Options', 'ALLOW FROM https://example.com/');
+    //     $response->headers->set('X-Frame-Options', 'ALLOW FROM url', false);
+    //     // $response->header('X-Frame-Options', 'SAMEORIGIN');
+    //     return $response;
+    // }
     public function handle($request, Closure $next)
     {
-        //return $next($request);
         $response = $next($request);
-        // $response->header('X-Frame-Options', 'ALLOW FROM https://example.com/');
-        $response->header('X-Frame-Options', 'SAMEORIGIN');
+
+        $option = 'SAMEORIGIN';
+
+        // In this example, we are only allowing the third party to include the "iframe" route
+        // It's always better to scope this to a given route / set of routes to avoid any unattended security problems
+        if ($xframeOptions = env('X_FRAME_OPTIONS', 'SAMEORIGIN')) {
+            if (false !== strpos($xframeOptions, 'ALLOW-FROM')) {
+                $url = trim(str_replace('ALLOW-FROM', '', $xframeOptions));
+
+                $response->header('Content-Security-Policy', 'frame-ancestors '.$url);
+            }
+        }
+
+        $response->header('X-Frame-Options', $xframeOptions);
+
         return $response;
     }
 }
