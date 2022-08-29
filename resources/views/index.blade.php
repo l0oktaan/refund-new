44617ee69e224712ae364e7dcd836389d86e6bab
l0oktaan <looktaan.tc3@gmail.com>
2022-08-29 23:02:00 +0700

    DELL 650829-2301

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,20 +10,20 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'e-Withdraw') }}</title>
 
-    <!-- Styles -->
-    <!-- <link rel="stylesheet" type="text/css" href="{{asset('css/app.css')}}"> -->
+
+
     <link rel="stylesheet" type="text/css" href="/css/app.css">
-    
+
 </head>
 <body>
     <div id="app">
         <router-view></router-view>
     </div>
     <script type="text/javascript" src="/js/app.js"></script>
-    <!-- <script type="text/javascript" src="{{asset('js/app.js')}}"></script> -->
-    
+
+
 
 </body>
 </html>
