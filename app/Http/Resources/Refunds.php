72fceb9afd0b6e3241ede6f64d4150f4c5dddfdf
l0oktaan <looktaan.tc3@gmail.com>
2023-03-02 14:36:28 +0700

    660302-1436 contract shedule edit

--- app/Http/Resources/Refunds.php
+++ app/Http/Resources/Refunds.php
@@ -43,6 +43,7 @@ class Refunds extends Resource
                 'complete_by' => $this->complete_by,
                 'complete_description' => $this->complete_description,
                 'contracts' => ContractResource::collection($this->contracts),
+                'contract_schedule_edits' => ContractScheduleEditResource::collection($this->contract_schedule_edits),
                 'contract_edits' => ContractBudgetEditResource::collection($this->contract_budget_edits),
                 'delivers' => DeliverResource::collection($this->delivers),
                 'status' => $this->status,
